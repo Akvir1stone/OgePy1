ed441828ab6571b597ec110eca62b120ce4c596b
Akvir1stone <119800067+Akvir1stone@users.noreply.github.com>
2023-09-15 11:59:15 +0300

    1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-amount_of_numbers = input('Vvedite kol-vo chisel: ')
+amount = input('Vvedite kol-vo chisel: ')
 max = 0
 i = 1
-for i in range(0, int(amount_of_numbers)):
+for i in range(0, int(amount)):
     i + 1
     number = int(input('Vvedite chislo: '))
     if number > max and number % 5 == 0:
